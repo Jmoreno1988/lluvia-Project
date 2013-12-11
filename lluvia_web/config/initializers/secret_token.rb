d18d34b090f115227754cdd40ba511545d7f6ff3
git-username <git-email-address>
2013-12-11 17:48:12 +0100

    upgrade lluvia_web

---
+++ lluvia_web/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LluviaWeb::Application.config.secret_key_base = 'c855e7631f69c386aea1089449fed4650a0fde6793064a0c3fd249ad44ae49f372db37187390e21ec4740f04953b3f2e1d1ce049ebfc74d79871f711424412cc'
